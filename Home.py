ac2514c1cf307a8080dc17e5c183aefca77467d4
Jimmy <jimmydzj2006@gmail.com>
2024-10-07 22:40:12 -0400

    streamlit multi-page

---
+++ Home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    O Canada!  
+    Our home and native land!  
+    True patriot love in all of us command.  
+    With glowing hearts we see thee rise,  
+    The True North strong and free!  
+    From far and wide,  
+    O Canada, we stand on guard for thee.  
+    God keep our land glorious and free!  
+    O Canada, we stand on guard for thee.  
+    O Canada, we stand on guard for thee.  
+"""
+)
